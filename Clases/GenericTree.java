bffa9df1fb04ce3e8c6abab1ce2817c795a72e66
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-04-04 18:42:10 -0600

    Create GenericTree.java

---
+++ Clases/GenericTree.java
@@ -0,0 +1,11 @@
+/**
+ * Hoja de Trabajo 7
+ * Maria Marta Ramirez - 21342
+ * Algoritmos y Estructura de Datos
+ * 
+ * Clase GenericBinarySearchTree
+ */
+
+public class GenericBST<V ,K> {
+
+    }
