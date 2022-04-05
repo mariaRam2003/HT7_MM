fda0bc23a0e676c3888ff18ce5193d79bcbd63c1
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-04-04 18:42:59 -0600

    Create Main.java

---
+++ Clases/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Hoja de Trabajo 7
+ * Maria Marta Ramirez - 21342
+ * Algoritmos y Estructura de Datos
+ * 
+ * Clase Main
+ */
+
+public class Main{
+    public static void main(String[] args) {
+
+    }
+}
