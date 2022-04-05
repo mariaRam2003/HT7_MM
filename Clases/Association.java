2b20ea0228888361b7b614a9f79373984586188e
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-04-04 19:42:24 -0600

    Update Association.java

--- Clases/Association.java
+++ Clases/Association.java
@@ -7,5 +7,23 @@
  */
 
 public class Association<V,K> {
-    
+    public V key;
+    public K value;
+    public Association(V KEY, K VALUE) {
+        key = KEY;
+        value = VALUE;
+    }
+    public V getKey() {
+        return key;
+    }
+    public K getValue() {
+        return value;
+    }
+    public void setKey(V key) {
+        this.key = key; 
+    }
+    public void setValue(K value) {
+        this.value = value;
+    }
+
 }
