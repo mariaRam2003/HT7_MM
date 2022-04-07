8c37a0c05e2a01477699c72c7147da477c0d864c
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-04-06 22:02:12 -0600

    Create TestBTree.java

---
+++ Clases/TestBTree.java
@@ -0,0 +1,18 @@
+/**
+ * Hoja de Trabajo 7
+ * @author Maria Marta Ramirez
+ * Carne 21342
+ * Algoritmos y Estructura de Datos
+ * 
+ * Test de la clase GenericTree (Arbol Binario de Busqueda)
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestBTree{
+    @Test
+    public void searchTest(){
+
+    }
+}
